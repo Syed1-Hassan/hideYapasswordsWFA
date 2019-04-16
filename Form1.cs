4e6d48e24acf6d598cdb3e5add69beb650b05061
Syed1-Hassan <rummanali109@gmail.com>
2019-04-16 15:32:12 +0500

    updated design

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace hideYaPasswordWFA
 {
-    public partial class Form1 : Form
+    public partial class hideYApasswords : Form
     {
-        public Form1()
+        public hideYApasswords()
         {
             InitializeComponent();
         }
