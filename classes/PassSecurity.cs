f0eaf0bff9982c7be5babfe1a04ed06d144b2343
Syed1-Hassan <rummanali109@gmail.com>
2019-04-24 03:12:48 +0500

    code added but not integrated in the application

---
+++ classes/PassSecurity.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+namespace hideYApasswordsWFA.classes
+{
+    class PassSecurity
+    {
+        public void savePasscode(string passcode, Dictionary<char, char> passKey, string dir)
+        {
+            encrypt code = new encrypt();
+            string encPasscode = code.funcEncrypt(passcode, passKey);
+            code.saveEncrypt("Passcode", encPasscode, dir);
+        }
+        public string decryptPasscode(string passcode, Dictionary<char, char> passKey)
+        {
+            string decPasscode = "";
+            decrypt code = new decrypt();
+            return decPasscode = code.funcDecrypt(passcode, passKey);
+        }
+
+        public bool passcodeINfile(string dir)
+        {
+            StreamReader reader = new StreamReader(dir);
+            Regex reg = new Regex(@"^passcode \: \w {3}$");
+            string line = reader.ReadLine();
+            Match passcodeMatch = reg.Match(line);
+            return (passcodeMatch.Success);
+
+        }
+        public string enterPasscode()
+        {
+            string pass;
+
+            do
+            {
+                pass = "123";
+               
+
+            } while (pass.Length >= 4);
+            return pass;
+        }
+        public string fetchPasscodeFromDirectory(string dir)
+        {
+            string passCode = "";
+            StreamReader reader = new StreamReader(dir);
+            string line = reader.ReadLine();
+            foreach (var s in Regex.Split(line, @"Passcode : "))
+            {
+                passCode = passCode + s;
+            }
+
+
+            return passCode;
+
+        }
+
+        public string fetchPasswordFromDirectory(string dir, string email)
+        {
+            string passWord = "";
+            StreamReader reader = new StreamReader(dir);
+            Regex reg = new Regex(email + @".+$");
+            string line = reader.ReadToEnd();
+            Match emailMatch = reg.Match(line);
+            if (emailMatch.Success)
+            {
+                string Fetchemail = emailMatch.ToString();
+                foreach (var s in Regex.Split(Fetchemail, @"^.+\@\w+\.com : "))
+                {
+                    passWord = passWord + s;
+                }
+
+            }
+            else
+                passWord = "No match for email";
+            return passWord;
+
+        }
+    }
+}
---
+++ classes/encrypt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace hideYApasswordsWFA.classes
+{
+    class encrypt
+    {
+        public string funcEncrypt(string s, Dictionary<char, char> x)
+        {
+            string retEncrypt = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                if (x.ContainsKey(s[i]))
+                {
+                    retEncrypt = retEncrypt + x[s[i]];
+                }
+
+            }
+            return retEncrypt;
+        }
+
+        public void saveEncrypt(string email, string code, string filepath)
+        {
+            //SetAccessRule(filepath);
+            StreamWriter h = new StreamWriter(filepath, true);
+            h.WriteLine(email + " :  " + code);
+            h.Close();
+
+        }
+    }
+}
