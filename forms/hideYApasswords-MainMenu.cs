be0d5ec9f3fbcaf50ca164c650a8b5b08b85f2ef
Syed1-Hassan <rummanali109@gmail.com>
2019-10-31 17:32:39 +0500

    UI update 1--> new main form added

---
+++ forms/hideYApasswords-MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hideYApasswordWFA
+{
+    public partial class hideYApasswords_MainMenu : Form
+    {
+        public hideYApasswords_MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
